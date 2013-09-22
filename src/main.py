73ed015188ccc6716048383a408e6ad725a8ebfa
Brian Friesen <friesen@ou.edu>
2013-09-22 14:14:19 -0500

    indexing stuff was completely wrong, mostly fixed now

--- src/main.py
+++ src/main.py
@@ -103,59 +103,76 @@ from scipy.integrate import simps
 
 # build tri-diagonal component of Lambda matrix
 
-# TODO: check boundary conditions (j = 0 and j = n_depth_points). Did I miss anything?
-# TODO: come up with a cleaner way to show these indices
-
 Lambda_star = np.zeros([n_depth_pts, n_depth_pts])
-inorm_im1 = np.zeros(n_mu_pts)
-inorm_i   = np.zeros(n_mu_pts)
-inorm_ip1 = np.zeros(n_mu_pts)
+
+inorm_tmp = np.zeros([n_depth_pts, n_mu_pts])
 
 for l in range(1, n_depth_pts-2):
     for j, ray in enumerate(rays):
         ray_idx_l   = get_ray_index_for_grid_point(ray, l)
 
-        inorm_im1[j] =  ray.gamma(ray_idx_l-1)
-        inorm_i  [j] =  ray.gamma(ray_idx_l-1) * exp(-ray.Delta_tau(ray_idx_l-1)) + ray.beta(ray_idx_l)
-        inorm_ip1[j] = (ray.gamma(ray_idx_l-1) * exp(-ray.Delta_tau(ray_idx_l-1)) + ray.beta(ray_idx_l)) * exp(-ray.Delta_tau(ray_idx_l)) + ray.alpha(ray_idx_l+1)
+        grid_idx_lim1 = get_grid_index_for_ray_point(ray, ray_idx_l-1)
+        grid_idx_lip1 = get_grid_index_for_ray_point(ray, ray_idx_l+1)
 
-    Lambda_star[l-1, l] = 0.5 * simps(inorm_im1, mu_grid)
-    Lambda_star[l  , l] = 0.5 * simps(inorm_i  , mu_grid)
-    Lambda_star[l+1, l] = 0.5 * simps(inorm_ip1, mu_grid)
+        inorm_tmp[grid_idx_lim1, j] =  ray.gamma(ray_idx_l-1)
+        inorm_tmp[l]                =  ray.gamma(ray_idx_l-1) * exp(-ray.Delta_tau(ray_idx_l-1)) + ray.beta(ray_idx_l)
+        inorm_tmp[grid_idx_lip1, j] = (ray.gamma(ray_idx_l-1) * exp(-ray.Delta_tau(ray_idx_l-1)) + ray.beta(ray_idx_l)) * exp(-ray.Delta_tau(ray_idx_l)) + ray.alpha(ray_idx_l+1)
+
+    # TODO: these are hard-coded, works in 1-D plane parallel, but won't work in more complex geometries
+    Lambda_star[l-1, l] = 0.5 * simps(inorm_tmp[grid_idx_lim1, :], mu_grid)
+    Lambda_star[l  , l] = 0.5 * simps(inorm_tmp[l,             :], mu_grid)
+    Lambda_star[l+1, l] = 0.5 * simps(inorm_tmp[grid_idx_lip1, :], mu_grid)
 
 # boundary cases.
 
-# TODO: check that this is right. there are some index overflows that I just
-# manually set to zero to avoid errors, but I have no idea if this is right
+inorm_tmp[:, :] = 0
 
-inorm_im1[:] = 0
-inorm_i  [:] = 0
-inorm_ip1[:] = 0
+# surface
 l = 0
 for j, ray in enumerate(rays):
     # at the surface we don't have an "i-1" term on rays with mu < 0 because they start at the surface
     if ray.mu > 0:
         ray_idx_l = get_ray_index_for_grid_point(ray, l)
-        # TODO: some of these indices overflowed past the edge and I removed
-        # them by hand. No idea if this is right. It's suspicious that two
-        # matrix elements are identical...
-        inorm_i  [j] = ray.gamma(ray_idx_l-1) * exp(-ray.Delta_tau(ray_idx_l-1)) + ray.beta(ray_idx_l)
-        inorm_ip1[j] = ray.gamma(ray_idx_l-1) * exp(-ray.Delta_tau(ray_idx_l-1)) + ray.beta(ray_idx_l)
-Lambda_star[l,   l] = 0.5 * simps(inorm_i  , mu_grid)
-Lambda_star[l+1, l] = 0.5 * simps(inorm_ip1, mu_grid)
 
-inorm_im1[:] = 0
-inorm_i  [:] = 0
-inorm_ip1[:] = 0
+        grid_idx_im1 = get_grid_index_for_ray_point(ray, ray_idx_l-1)
+
+        inorm_tmp[grid_idx_lim1, j] = ray.gamma(ray_idx_l-1)
+        inorm_tmp[l,             j] = ray.gamma(ray_idx_l-1) * exp(-ray.Delta_tau(ray_idx_l-1)) + ray.beta(ray_idx_l)
+    else:
+        # no illumination from the surface
+        ray_idx_l = get_ray_index_for_grid_point(ray, l)
+
+        grid_idx_ip1 = get_grid_index_for_ray_point(ray, ray_idx_l+1)
+
+        inorm_tmp[l,             j] = 0
+        inorm_tmp[grid_idx_lip1, j] = ray.alpha(ray_idx_l+1)
+
+Lambda_star[l,   l] = 0.5 * simps(inorm_tmp[l,             :], mu_grid)
+Lambda_star[l+1, l] = 0.5 * simps(inorm_tmp[grid_idx_lip1, :], mu_grid)
+
+inorm_tmp[:, :] = 0
+
+# depth
 l = n_depth_pts-1
 for j, ray in enumerate(rays):
     # at depth, we don't have an "i-1" term on rays with mu > 0 because they start at depth
     if (ray.mu < 0):
         ray_idx_l    = get_ray_index_for_grid_point(ray, l)
-        inorm_im1[j] = ray.gamma(ray_idx_l-1)
-        inorm_i  [j] = ray.gamma(ray_idx_l-1) * exp(-ray.Delta_tau(ray_idx_l-1)) + ray.beta(ray_idx_l)
-Lambda_star[l-1, l] = 0.5 * simps(inorm_im1, mu_grid)
-Lambda_star[l  , l] = 0.5 * simps(inorm_i  , mu_grid)
+
+        grid_idx_im1 = get_grid_index_for_ray_point(ray, ray_idx_l-1)
+
+        inorm_tmp[grid_idx_lim1, j] = ray.gamma(ray_idx_l-1)
+        inorm_tmp[l,             j] = ray.gamma(ray_idx_l-1) * exp(-ray.Delta_tau(ray_idx_l-1)) + ray.beta(ray_idx_l)
+    else:
+        ray_idx_l    = get_ray_index_for_grid_point(ray, l)
+
+        grid_idx_ip1 = get_grid_index_for_ray_point(ray, ray_idx_l+1)
+
+        inorm_tmp[l,             j] = planck_fn(1)
+        inorm_tmp[grid_idx_lip1, j] = planck_fn(1) * exp(-ray.Delta_tau(ray_idx_l)) + ray.alpha(ray_idx_l+1)
+
+Lambda_star[l-1, l] = 0.5 * simps(inorm_tmp[grid_idx_lim1, :], mu_grid)
+Lambda_star[l  , l] = 0.5 * simps(inorm_tmp[l,             :], mu_grid)
 
 print (Lambda_star)
 
@@ -168,7 +185,7 @@ def calc_J(rays):
     for i in range(n_depth_pts):
         for j, ray in enumerate(rays):
             I_mu[j] = ray.I_lam[i]
-        J_lam[i] = simps(I_mu, mu_grid)
+        J_lam[i] = 0.5 * simps(I_mu, mu_grid)
     return (J_lam)
 
 J_n   = np.empty(n_depth_pts)
@@ -185,3 +202,4 @@ epsilon = 1.0e-4
 for i in range(10):
     J_np1 = J_n + np.dot(np.linalg.inv(1 - (1 - epsilon)*Lambda_star), (J_fs - J_n))
     J_n = J_np1
+    print(J_n)
