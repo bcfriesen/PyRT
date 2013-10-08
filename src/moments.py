c68740e0757a925d8d6e407d007229916f789e8b
Brian Friesen <friesen@ou.edu>
2013-10-08 14:18:51 -0500

    fixed index error in calc_J

--- src/moments.py
+++ src/moments.py
@@ -8,6 +8,6 @@ def calc_J(rays, n_mu_pts, n_depth_pts, mu_grid):
     J_lam = np.zeros(n_depth_pts)
     for i in range(n_depth_pts):
         for j, each_ray in enumerate(rays):
-            I_mu[j] = each_ray.I_lam[get_ray_index_for_grid_point(each_ray, j, n_depth_pts)]
+            I_mu[j] = each_ray.I_lam[get_ray_index_for_grid_point(each_ray, i, n_depth_pts)]
         J_lam[i] = 0.5 * simps(I_mu, mu_grid)
     return (J_lam)
