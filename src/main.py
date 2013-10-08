72ebcc9e531e0a086e2ad49750800fb47d311360
Brian Friesen <friesen@ou.edu>
2013-10-08 12:21:31 -0500

    moved imports to just before where they are actually used (for clarity)

--- src/main.py
+++ src/main.py
@@ -4,12 +4,10 @@ n_mu_pts = 10
 # number of physical grid depth points
 n_depth_pts = 100
 
-import numpy as np
-from planck import planck_fn
-
 # thermalization parameter. 1 = LTE; 0 = pure scattering
 epsilon = 1.0e-4
 
+import numpy as np
 # mean intensity
 J_n   = np.zeros(n_depth_pts)
 # initial "guess" for J
@@ -19,27 +17,20 @@ J_n[:]  = 2
 source_fn = np.zeros(n_depth_pts)
 
 from source_fn import calc_source_fn
-
 calc_source_fn(source_fn, epsilon, J_n)
 
 import astropy.units as u
-
 # physical grid
 radial_grid = np.linspace(1, 10, n_depth_pts) * u.cm
 
 # opacity grid
 chi_grid = np.logspace(-7, 3, n_depth_pts) * (1/u.cm)
 
-from grid_functions import *
-
-from math import fabs, exp
-
-from ray import ray
-
 # angular grid
 mu_grid = np.linspace(-1, 1, n_mu_pts)
 
 rays = []
+from ray import ray
 for mu in mu_grid:
     rays.append(ray(mu, n_depth_pts, radial_grid))
 
@@ -48,14 +39,13 @@ for ray in rays:
     ray.calc_tau(n_depth_pts, radial_grid, chi_grid)
     ray.formal_soln(n_depth_pts, source_fn)
 
-from scipy.integrate import simps
 
 # build tri-diagonal component of Lambda matrix
-
 Lambda_star = np.zeros([n_depth_pts, n_depth_pts])
-
 inorm_tmp = np.zeros([n_depth_pts, n_mu_pts])
-
+from grid_functions import *
+from math import exp
+from scipy.integrate import simps
 for l in range(1, n_depth_pts-2):
     for j, ray in enumerate(rays):
         # get ray index corresponding to physical grid index l
@@ -105,6 +95,7 @@ Lambda_star[l+1, l] = 0.5 * simps(inorm_tmp[grid_idx_lip1, :], mu_grid)
 inorm_tmp[:, :] = 0
 
 # depth
+from planck import planck_fn
 l = n_depth_pts-1
 for j, ray in enumerate(rays):
     # at depth, we don't have an "i-1" term on rays with mu > 0 because they start at depth
@@ -128,12 +119,11 @@ Lambda_star[l  , l] = 0.5 * simps(inorm_tmp[l,             :], mu_grid)
 
 # mean intensity
 
-from moments import calc_J
-
 J_np1 = np.zeros(n_depth_pts)
 J_fs  = np.zeros(n_depth_pts)
 
 # J from formal solution (calculated earlier)
+from moments import calc_J
 J_fs[:] = calc_J(rays, n_mu_pts, n_depth_pts, mu_grid)
 
 import matplotlib.pyplot as plt
