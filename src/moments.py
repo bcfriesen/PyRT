9df36276e12ecfadb9796577a7a615125e3c601e
Brian Friesen <friesen@ou.edu>
2013-10-08 14:18:09 -0500

    renamed a missed ambiguous iterator name "ray"

--- src/moments.py
+++ src/moments.py
@@ -7,7 +7,7 @@ def calc_J(rays, n_mu_pts, n_depth_pts, mu_grid):
     I_mu = np.zeros(n_mu_pts)
     J_lam = np.zeros(n_depth_pts)
     for i in range(n_depth_pts):
-        for j, ray in enumerate(rays):
-            I_mu[j] = ray.I_lam[get_ray_index_for_grid_point(ray, j, n_depth_pts)]
+        for j, each_ray in enumerate(rays):
+            I_mu[j] = each_ray.I_lam[get_ray_index_for_grid_point(each_ray, j, n_depth_pts)]
         J_lam[i] = 0.5 * simps(I_mu, mu_grid)
     return (J_lam)
