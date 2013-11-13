d0c2b270fe0654d57b7680f1478e79ec4d71519b
Brian Friesen <friesen@ou.edu>
2013-11-13 13:31:36 -0600

    ALI looks exactly the way it does in OAB86 and OK87, but still doesn't give the correct answer...

--- src/main.py
+++ src/main.py
@@ -1,13 +1,14 @@
+import numpy as np
+np.set_printoptions(linewidth=200)
+
 # number of angle points
 n_mu_pts = 10
 
 # number of physical grid depth points
-n_depth_pts = 100
+n_depth_pts = 10
 
 # thermalization parameter. 1 = LTE; 0 = pure scattering
 epsilon = 1.0e-3
-
-import numpy as np
 
 # "old" source function estimate
 source_fn_n = np.zeros(n_depth_pts)
@@ -46,26 +47,26 @@ Lambda_star = np.zeros([n_depth_pts, n_depth_pts])
 from calc_Lambda_star import calc_Lambda_star
 Lambda_star = calc_Lambda_star(n_depth_pts, n_mu_pts, rays, mu_grid)
 
-# mean intensity from the formal solution
-source_fn_fs = np.zeros(n_depth_pts)
+# get source function from the formal solution
 from source_fn import calc_source_fn
-source_fn_fs = calc_source_fn(epsilon, J_fs)
+source_fn_n = calc_source_fn(epsilon, J_fs)
 
 planck_grid = np.zeros(n_depth_pts)
 for i in range(len(planck_grid)):
     planck_grid[i] = planck_fn(1)
+
+planck_grid = np.zeros(n_depth_pts)
+planck_grid[:] = planck_fn(1)
 
 import matplotlib.pyplot as plt
 fig = plt.figure()
 ax = fig.add_subplot(111)
 Delta_S = np.zeros([n_depth_pts])
 for i in range(50):
-    Delta_S = np.linalg.solve(1.0 - (1.0 - epsilon) * Lambda_star, source_fn_fs - source_fn_n)
-    source_fn_np1 = source_fn_n + Delta_S
     for each_ray in rays:
-        each_ray.formal_soln(n_depth_pts, source_fn_np1)
+        each_ray.formal_soln(n_depth_pts, source_fn_n)
     J_fs = calc_J(rays, n_mu_pts, n_depth_pts, mu_grid)
-    source_fn_fs = calc_source_fn(epsilon, J_fs)
+    source_fn_np1 = np.linalg.solve(1.0 - (1.0 - epsilon) * Lambda_star, (1.0 - epsilon) * (J_fs - np.dot(Lambda_star, source_fn_n)) + epsilon*planck_grid)
     ax.plot(chi_grid, J_fs)
     source_fn_n = source_fn_np1
 ax.set_xscale('log')
